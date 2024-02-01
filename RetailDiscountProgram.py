906387f6cc7297990b7d41202ba08883c9e6919b
Taylor Martin <taylormartind@gmail.com>
2024-02-01 15:37:20 -0500

    pull initial commit from main branch

---
+++ RetailDiscountProgram.py
@@ -0,0 +1,10 @@
+# Menu Page
+print("-----------------------------------\n"
+      "|   Welcome back, Taylor!         |\n"
+      "-----------------------------------\n"
+      "|   1 - Create a New Employee     |\n"
+      "|   2 - Create an Item            |\n"
+      "|   3 - Make a Purchase           |\n"
+      "|   4 - All Employee Summary      |\n"
+      "|   5 - Exit                      |\n"
+      "-----------------------------------")
