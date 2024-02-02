08c9549ca7c51819656c0d5a35e0c15cc7adcb5b
Taylor Martin <taylormartind@gmail.com>
2024-02-02 15:00:21 -0500

    update employee_id

--- RetailDiscountProgram.py
+++ RetailDiscountProgram.py
@@ -16,15 +16,20 @@ menuSelection = input("|   Enter Menu Option Selection:       |")
 employee_list = []
 
 # Create a New Employee Function
-def create_employee():
-    # employee_id creation + validation
-    while True:
-        employee_id = (input("|   Enter 4-Digit Employee ID: "))
-        if employee_id.isnumeric() and len(employee_id) == 4:
-            break
-        print("|   Error! Employee ID must be 4-digit integer.")
+def createEmployee():
+    global employee_type
 
-    #employee_discount creation + validation
+    while True:
+        try:
+            employee_id = int(input("|   Enter 4-Digit Employee ID: "))
+            if len(str(employee_id)) == 4:
+                break
+            else:
+                print("|   Error! Employee ID must be 4-digit integer.")
+        except ValueError:
+            print("|   Error! Employee ID must be 4-digit integer.")
+
+    # employee_discount creation + validation
     while True:
         employee_name = (input("|   Enter Employee First and Last Name: "))
         if re.match(r'^[a-zA-Z]+([\' -][a-zA-Z]+)*$', employee_name):
@@ -37,6 +42,15 @@ def create_employee():
         print("|   Error! Employee Type must be Hourly or Manager.")
         employee_type = (input("|   Enter Employee Type (Hourly/Manager): "))
 
-create_employee()
+    # years_worked creation + validation
+    while True:
+        try:
+            years_worked = int(input("|   Enter Number of Years Employee has Worked: "))
+            break
+        except ValueError:
+            print("|   Error! Employee Years must be an integer.")
+
+
+createEmployee()
 
 
--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 import re
 def createEmployee():
     global employee_type
+
     while True:
-        employee_id = (input("|   Enter 4-Digit Employee ID: "))
-        if employee_id.isnumeric() and len(employee_id) == 4:
-            break
-        print("|   Error! Employee ID must be 4-digit integer.")
+        try:
+            employee_id = int(input("|   Enter 4-Digit Employee ID: "))
+            if len(str(employee_id)) == 4:
+                break
+            else:
+                print("|   Error! Employee ID must be 4-digit integer.")
+        except ValueError:
+            print("|   Error! Employee ID must be 4-digit integer.")
 
     # employee_discount creation + validation
     while True:
@@ -14,11 +19,19 @@ def createEmployee():
             break
         print("|   Error! Employee name must be alphanumeric.")
 
-# employee_type creation + validation
+    # employee_type creation + validation
     employee_type = (input("|   Enter Employee Type (Hourly/Manager): "))
     while employee_type.lower() not in ["hourly", "manager"]:
         print("|   Error! Employee Type must be Hourly or Manager.")
         employee_type = (input("|   Enter Employee Type (Hourly/Manager): "))
+
+    # years_worked creation + validation
+    while True:
+        try:
+            years_worked = int(input("|   Enter Number of Years Employee has Worked: "))
+            break
+        except ValueError:
+            print("|   Error! Employee Years must be an integer.")
 
 
 createEmployee()
