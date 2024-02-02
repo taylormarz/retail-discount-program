35c6ba2e111383778a9e70751afc34fe97dd92fe
Taylor Martin <taylormartind@gmail.com>
2024-02-02 15:34:07 -0500

    completed create employee func

--- RetailDiscountProgram.py
+++ RetailDiscountProgram.py
@@ -1,7 +1,12 @@
 import re
+import sys
+
+# List to store employee info
+employee_list = []
+
 # Menu Page
 menuDesign = ("----------------------------------------\n" +
-              "|   Welcome back, Taylor!              |\n" +
+              "|   Main Menu:                         |\n" +
               "----------------------------------------\n" +
               "|   1 - Create a New Employee          |\n" +
               "|   2 - Create an Item                 |\n" +
@@ -10,47 +15,60 @@ menuDesign = ("----------------------------------------\n" +
               "|   5 - Exit                           |\n" +
               "----------------------------------------")
 print(menuDesign)
-menuSelection = input("|   Enter Menu Option Selection:       |")
-
-# List to store employee info
-employee_list = []
 
 # Create a New Employee Function
-def createEmployee():
-    global employee_type
-
+def create_employee():
+    print("----------------------------------------\n"
+          "|   You are creating a new employee.   |\n"
+          "----------------------------------------")
     while True:
         try:
-            employee_id = int(input("|   Enter 4-Digit Employee ID: "))
+            employee_id = int(input("    Enter 4-Digit Employee ID: "))
             if len(str(employee_id)) == 4:
                 break
             else:
-                print("|   Error! Employee ID must be 4-digit integer.")
+                print("    Error! Employee ID must be 4-digit integer.")
         except ValueError:
-            print("|   Error! Employee ID must be 4-digit integer.")
+            print("    Error! Employee ID must be 4-digit integer.")
 
     # employee_discount creation + validation
     while True:
-        employee_name = (input("|   Enter Employee First and Last Name: "))
+        employee_name = (input("    Enter Employee First and Last Name: "))
         if re.match(r'^[a-zA-Z]+([\' -][a-zA-Z]+)*$', employee_name):
             break
-        print("|   Error! Employee name must be alphanumeric.")
+        print("    Error! Employee name must be alphanumeric.")
 
     # employee_type creation + validation
-    employee_type = (input("|   Enter Employee Type (Hourly/Manager): "))
+    employee_type = (input("    Enter Employee Type (Hourly/Manager): "))
     while employee_type.lower() not in ["hourly", "manager"]:
-        print("|   Error! Employee Type must be Hourly or Manager.")
-        employee_type = (input("|   Enter Employee Type (Hourly/Manager): "))
+        print("    Error! Employee Type must be Hourly or Manager.")
+        employee_type = (input("    Enter Employee Type (Hourly/Manager): "))
 
     # years_worked creation + validation
     while True:
         try:
-            years_worked = int(input("|   Enter Number of Years Employee has Worked: "))
+            years_worked = int(input("    Enter Number of Years Employee has Worked: "))
             break
         except ValueError:
-            print("|   Error! Employee Years must be an integer.")
+            print("    Error! Employee Years must be an integer.")
+
+    # employee_purchases creation + set to 0 to start
+    employee_purchases = 0
+
+    # employee_discount creation + set to 0 to start
+    employee_discount = 0
 
 
-createEmployee()
+# Menu Selection Function
+def menu_options():
+    while True:
+        menu_selection = input("    Enter Menu Option Selection: ")
+        if menu_selection == "1":
+            create_employee()
+        elif menu_selection == "5":
+            sys.exit()
+        print(menuDesign)
 
+
+menu_options()
 
