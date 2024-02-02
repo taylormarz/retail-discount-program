7a757ad6d2a4ac0c2ca985bd643c22c984096c2a
Taylor Martin <taylormartind@gmail.com>
2024-02-02 14:40:28 -0500

    new employee function updates

--- RetailDiscountProgram.py
+++ RetailDiscountProgram.py
@@ -1,3 +1,4 @@
+import re
 # Menu Page
 menuDesign = ("----------------------------------------\n" +
               "|   Welcome back, Taylor!              |\n" +
@@ -11,6 +12,9 @@ menuDesign = ("----------------------------------------\n" +
 print(menuDesign)
 menuSelection = input("|   Enter Menu Option Selection:       |")
 
+# List to store employee info
+employee_list = []
+
 # Create a New Employee Function
 def create_employee():
     # employee_id creation + validation
@@ -20,6 +24,25 @@ def create_employee():
             break
         print("|   Error! Employee ID must be 4-digit integer.")
 
+    #employee_discount creation + validation
+    while True:
+        employee_name = (input("|   Enter Employee First and Last Name: "))
+        if re.match(r'^[a-zA-Z]+([\' -][a-zA-Z]+)*$', employee_name):
+            break
+        print("|   Error! Employee name must be alphanumeric.")
+
+    # employee_type creation + validation
+    while employee_type not in ["Hourly", "Manager"]:
+        employee_type = (input("|   Enter Employee Type (Hourly/Manager): "))
+        print("|   Error! Employee Type must be Hourly or Manager.")
+        if employee_type == "Hourly" or employee_type == "Manager":
+            break
+
+    # employee_type creation + validation
+    employee_type = (input("|   Enter Employee Type (Hourly/Manager): "))
+    while employee_type.lower() not in ["hourly", "manager"]:
+        print("|   Error! Employee Type must be Hourly or Manager.")
+        employee_type = (input("|   Enter Employee Type (Hourly/Manager): "))
 
 create_employee()
 
